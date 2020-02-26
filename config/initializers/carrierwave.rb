b15c806261b02055b52f8b566e159b068477c7c4
macoto-k <sakura.0830.m@gmail.com>
2020-02-26 21:33:55 +0900

    再提出６

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'ここにバケット名を入れます'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ここにバケット名を入れます'
+  config.fog_directory  = 'maico09'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/maico09'
 end
