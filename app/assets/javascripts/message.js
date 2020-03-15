572ad40e38480b969c7b5ec976946aa232c4791c
macoto-k <sakura.0830.m@gmail.com>
2020-03-15 18:01:46 +0900

    chatspace再提出

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function(){ 
-  last_message_id = $('.message:last').data("message-id");
-  
+  console.log("aaa")
+// last_message_id = $('.message:last').data("message-id");
   function buildHTML(message){
   
     if ( message.image ) {
@@ -42,6 +42,7 @@ $(function(){
       return html;
     };
   };
+
   
 
 $('#new_message').on('submit', function(e){
@@ -66,11 +67,14 @@ $('#new_message').on('submit', function(e){
     }
   })
   .fail(function() {
+    alert('error')
   });
-　})
+})
+
 
 var reloadMessages = function() {
-  last_message_id = $('.message:last').data("message-id");
+var last_message_id = $('.message:last').data("message-id");
+console.log(last_message_id)
   $.ajax({
     //ルーティングで設定した通りのURLを指定
     url: "api/messages",
@@ -81,22 +85,24 @@ var reloadMessages = function() {
     data: {id: last_message_id}
   })
   .done(function(messages) {
-    if (messages.length !== 0) {
-      //追加するHTMLの入れ物を作る
-      var insertHTML = '';
-      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-      $.each(messages, function(i, message) {
-        insertHTML += buildHTML(message)
-      });
-      //メッセージが入ったHTMLに、入れ物ごと追加
-      $('.messages').append(insertHTML);
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-    }
-  })
-  .fail(function() {
-    alert('error');
-  });
-
- };
-
-})
+    console.log(messages)
+     if (messages.length !== 0) {
+       var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+       $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+       });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.chat-body').append(insertHTML);
+        $('.chat-body').animate({ scrollTop: $('.chat-body')[0].scrollHeight});
+      }
+   })
+   .fail(function() {
+     alert('error');
+   });
+  }
+     if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+      setInterval(reloadMessages, 7000);
+      console.log("test")
+   };
+});
