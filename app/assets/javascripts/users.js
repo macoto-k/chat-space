cb48befeca9b84c648b67e0f046aefa53a87b5ae
macoto-k <sakura.0830.m@gmail.com>
2020-03-21 14:12:53 +0900

    chatspace再提出３

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,7 +2,6 @@ $(function() {
   function addUser(user) {
     let html = `
       <div class="chat-group-user clearfix">
-        <input name='group[user_ids][]' type='hidden' value='ユーザーのid'>
         <p class="chat-group-user__name">${user.name}</p>
         <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
       </div>
